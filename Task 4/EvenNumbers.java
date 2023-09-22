918e61edffef2400fab89e40a465b3aadc2d5ced
srujith <srujithmaddiboyina@gmail.com>
2023-09-22 09:46:36 +0530

    srujith 22-09-2023

---
+++ Task 4/EvenNumbers.java
@@ -0,0 +1,16 @@
+public class EvenNumbers 
+{
+    public static void main(String[] args) 
+    {
+        int start = 1;
+        int end = 30;
+        System.out.println("Even numbers from " + start + " to " + end + ":");
+        for (int i = start; i <= end; i++) 
+        {
+            if (i % 2 == 0) 
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}

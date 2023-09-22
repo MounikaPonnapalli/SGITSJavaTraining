c503e257e743cfbda3a2f83e848dbf577119275f
Karthik Yerramsetti <Karthikyerramsetti98@gmail.com>
2023-09-22 08:32:38 +0530

    21-09-2023

---
+++ Task4/MultiplicationNumbers.java
@@ -0,0 +1,15 @@
+package Task4;
+
+public class MultiplicationNumbers {
+    public static void main(String[] args)
+    {
+        for(int i=23;i<=28;i++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+            System.out.println(" ");
+        }
+    }
+}    

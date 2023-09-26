a7ef6a990202314cf9586b208cc0ed40fc865633
srujith <srujithmaddiboyina@gmail.com>
2023-09-26 15:58:14 +0530

    26-09-2023

--- ForEachloop.java
+++ ForEachloop.java
@@ -1,14 +1,27 @@
-public class ForEachloop {
-    public static void main(String args[]){
-     
-      int[] numbers = {3, 4, 5, -5, 0, 12};
-      int sum = 0;
-      for (int number: numbers) {
-        sum += number;
-      }
-     
-      System.out.println("Sum = " + sum);
-    }
-   }
-    
+package javapractice;
 
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6,-1,-3,-4};
+        int psum=0,nsum=0;
+		
+		for (int n1:arr) 
+		{
+		    if (n1 > 0)
+		    {
+		        psum += n1;
+		    } else if (n1 < 0) 
+		    {
+		        nsum += n1;
+		    }
+		}
+
+	        
+	        System.out.println("Sum of positive elements: " + psum);
+	        System.out.println("Sum of negative elements: " + nsum);
+
+
+	}
+
+}
